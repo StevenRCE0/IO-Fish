6a5a820d871d386fdaddbeee8d67dd798f17799b
StevenRCE0 <40051361+StevenRCE0@users.noreply.github.com>
2022-09-28 09:18:27 +0800

    Renaming

--- packages/website/app/root.tsx
+++ packages/website/app/root.tsx
@@ -43,7 +43,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
 	charset: 'utf-8',
-	title: 'IOLab 2022',
+	title: 'I/O Lab UI/UX Course',
 	viewport: 'width=device-width,initial-scale=1',
 })
 
