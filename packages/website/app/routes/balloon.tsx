e3d9b25f9b72f9d89ee91b40097b06857ece8ee0
StevenRCE0 <40051361+StevenRCE0@users.noreply.github.com>
2022-10-01 17:39:27 +0800

    Good rotation

--- packages/website/app/routes/balloon.tsx
+++ packages/website/app/routes/balloon.tsx
@@ -3,100 +3,126 @@ import type { CSSProperties } from 'react';
 import React from 'react';
 
 class SmallBalloon {
-	constructor(public styles: CSSProperties) {}
+    constructor(public styles: CSSProperties) {}
 }
 interface BalloonProps {}
 interface BalloonState {
-	speed: number;
-	decay: number;
-	currentRotation: number;
-	balloons: SmallBalloon[];
+    timer?: NodeJS.Timer;
+    speed: number;
+    decay: number;
+    maxBalloons: number;
+    currentRotation: number;
+    rotationStep: number;
+    balloons: SmallBalloon[];
 }
 
 const balloonWrapperStyle: CSSProperties = {
-	position: 'fixed',
-	width: '100vw',
-	height: '100vh',
+    position: 'fixed',
+    width: '100vw',
+    height: '100vh',
 };
 
 class Balloon extends React.Component<BalloonProps, BalloonState> {
-	constructor(props: BalloonProps) {
-		super(props);
-		this.state = {
-			speed: 1,
-			decay: 1,
-			currentRotation: 0,
-			balloons: [],
-		};
-	}
+    constructor(props: BalloonProps) {
+        super(props);
+        this.state = {
+            speed: 1,
+            decay: 1,
+            maxBalloons: 6,
+            currentRotation: 0,
+            rotationStep: 256 * 30 / 360,
+            balloons: [],
+        };
+    }
 
-	onComponentDidMount() {
-		this.balloonMachine();
-	}
+    componentDidMount() {
+        this.balloonMachine();
+    }
 
-	balloonMachine() {
-		setTimeout(() => {
-			this.setState({
-				currentRotation: this.state.currentRotation + 1,
-				balloons: this.state.balloons.concat(
-					new SmallBalloon(this.balloonStyleGenerator()),
-				),
-			});
-		}, 1000 / this.state.speed);
-		setTimeout(() => {
-			this.setState({
-				balloons: this.state.balloons.slice(1),
-			});
-		}, 1000 / this.state.speed + 1000 / this.state.decay);
-	}
+    componentWillUnmount(): void {
+        if (this.state.timer) {
+            clearInterval(this.state.timer);
+        }
+    }
 
-	balloonStyleGenerator(): React.CSSProperties {
-		return {
-			'--balloonDuration': `${1000 / this.state.speed}ms`,
-			fill: `hsl(${255 / this.state.currentRotation}, 100%, 80%)`,
-			stroke: 'rgb(60,88,150)',
-			strokeWidth: '1.51px',
-		} as React.CSSProperties;
-	}
+    reversedArrayCopy<T>(array: T[]): T[] {
+        const copy = [...array];
+        copy.reverse();
+        return copy;
+    }
 
-	render() {
-		return (
-			<div style={balloonWrapperStyle}>
-				{this.state.balloons.map((balloon, index) => {
-					return (
-						<svg
-							key={index}
-							width="100%"
-							height="100%"
-							viewBox="0 0 2517 2656"
-							version="1.1"
-							xmlns="http://www.w3.org/2000/svg"
-							xmlnsXlink="http://www.w3.org/1999/xlink"
-							xmlSpace="preserve"
-							style={{
-								fillRule: 'evenodd',
-								clipRule: 'evenodd',
-								strokeLinecap: 'round',
-								strokeLinejoin: 'round',
-								strokeMiterlimit: 1.5,
-							}}
-						>
-							<g transform="matrix(1,0,0,1,-1399,-1070)">
-								<g transform="matrix(0.660946,0.00174429,-0.00174429,0.660946,1096.21,1015.85)">
-									<path
-										d="M3633.97,3916.4C3580.88,3954.48 3523.56,3988.15 3454.81,4014.04C2412.54,4406.38 -275.263,3294.88 658.551,1116.38C776.462,841.304 1067.86,528.523 1324.17,363.757C1595.57,189.282 1954.16,81.663 2273,77.74C2953.57,69.364 3505.57,320.97 3919.93,992.046C4538.2,1993.35 4275.83,3455.95 3633.97,3916.4Z"
-										style={balloon.styles}
-									/>
-								</g>
-							</g>
-						</svg>
-					);
-				})}
-			</div>
-		);
-	}
+    balloonMachine() {
+            setInterval(() => {
+                const goodBalloons = (): SmallBalloon[] => {
+                    if (this.state.balloons.length > this.state.maxBalloons) {
+                        let newBalloons = this.state.balloons;
+                        newBalloons.shift();
+                        return newBalloons ?? [];
+                    } else {
+                        return this.state.balloons
+                    }
+                };
+                this.setState({
+                    balloons: this.state.balloons.filter(
+                        (balloon) => balloon.styles.top !== '100%',
+                    ),
+                });
+            this.setState({
+                currentRotation: this.state.currentRotation + 1,
+                balloons: goodBalloons().concat(
+                    new SmallBalloon(this.balloonStyleGenerator()),
+                ),
+            });
+        }, 1000 / this.state.speed);
+    }
+
+    balloonStyleGenerator(): React.CSSProperties {
+        return {
+            '--balloonDuration': `${1000 / this.state.speed}ms`,
+            fill: `hsl(${this.state.rotationStep * this.state.currentRotation}, 100%, 80%)`,
+            stroke: 'rgb(60,88,150)',
+            strokeWidth: '1.51px',
+        } as React.CSSProperties;
+    }
+
+    render() {
+        return (
+            <div style={balloonWrapperStyle}>
+                {this.reversedArrayCopy(this.state.balloons).map((balloon, index) => {
+                    return (
+                        <svg
+                            key={index}
+                            width="100%"
+                            height="100%"
+                            viewBox="0 0 2517 2656"
+                            version="1.1"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                            xmlSpace="preserve"
+                            style={{
+                                fillRule: 'evenodd',
+                                clipRule: 'evenodd',
+                                strokeLinecap: 'round',
+                                strokeLinejoin: 'round',
+                                strokeMiterlimit: 1.5,
+                            }}
+                        >
+                            <g transform="matrix(1,0,0,1,-1399,-1070)">
+                                <g transform="matrix(0.660946,0.00174429,-0.00174429,0.660946,1096.21,1015.85)">
+                                    <path
+                                        d="M3633.97,3916.4C3580.88,3954.48 3523.56,3988.15 3454.81,4014.04C2412.54,4406.38 -275.263,3294.88 658.551,1116.38C776.462,841.304 1067.86,528.523 1324.17,363.757C1595.57,189.282 1954.16,81.663 2273,77.74C2953.57,69.364 3505.57,320.97 3919.93,992.046C4538.2,1993.35 4275.83,3455.95 3633.97,3916.4Z"
+                                        style={balloon.styles}
+                                    />
+                                </g>
+                            </g>
+                        </svg>
+                    );
+                })}
+            </div>
+        );
+    }
 }
 
 export default function BalloonWrapper(props: BalloonProps) {
-	return <Balloon {...props} />;
+    return <Balloon {...props} />;
 }
