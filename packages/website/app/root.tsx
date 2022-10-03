e7cf37ef9d06677b0ac2f4e89701c817870af890
StevenRCE0 <40051361+StevenRCE0@users.noreply.github.com>
2022-10-03 13:54:50 +0800

    Adaptive paddings

--- packages/website/app/root.tsx
+++ packages/website/app/root.tsx
@@ -49,7 +49,7 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
     charset: 'utf-8',
     title: 'I/O Lab UI/UX Course',
-    viewport: 'width=device-width,initial-scale=1',
+    viewport: 'width=device-width,initial-scale=1,viewport-fit=cover',
 });
 
 export default function App() {
